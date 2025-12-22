dc4c2976fbbd64f68482017a924ee5810972c1cb
Michael H <misha.h@woof.software>
2025-12-22 19:40:48 +0100

    chore(owes-v3): split indexer db via Git LFS

--- src/indexer/indexer.service.ts
+++ src/indexer/indexer.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  Logger,
-  OnApplicationBootstrap,
-} from '@nestjs/common';
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 import { JsonRpcProvider, getAddress, ethers } from 'ethers';
